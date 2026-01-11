3145d6c9e22998d1d9b3c5b336d4523176786e6c
mark.van.leijenhorst <mark@droomploeg.nl>
2026-01-11 15:59:48 +0100

    Started with implementing of Audit

---
+++ src/Droomploeg.DreamOps.Infrastructure/Audit/ScopedAuditContextAccessor.cs
@@ -0,0 +1,17 @@
+﻿namespace Droomploeg.DreamOps.Infrastructure.Audit;
+
+public sealed class ScopedAuditContextAccessor : IAuditContextAccessor
+{
+    private IAuditContext? _context;
+
+    public ValueTask SetCurrentAsync(IAuditContext? context)
+    {
+        _context = context;
+        return ValueTask.CompletedTask;
+    }
+
+    public ValueTask<IAuditContext?> GetCurrentAsync()
+    {
+        return ValueTask.FromResult(_context);
+    }
+}
